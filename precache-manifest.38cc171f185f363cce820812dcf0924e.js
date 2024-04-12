878af9e3988c6585d80983fde5499eb6868f1d51
Peacebridge <73386395+Yeboah-Peacebridge-Osei@users.noreply.github.com>
2024-04-11 21:29:38 -0500

    Updates

---
+++ precache-manifest.38cc171f185f363cce820812dcf0924e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8702e5b186032910fcf4a8b77b3749b",
+    "url": "/InterviewWell/index.html"
+  },
+  {
+    "revision": "1c979b5aa6841757fb4e",
+    "url": "/InterviewWell/static/css/main.f1595baf.chunk.css"
+  },
+  {
+    "revision": "968acd4c6a24f994945d",
+    "url": "/InterviewWell/static/js/2.bfd517d8.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/InterviewWell/static/js/2.bfd517d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c979b5aa6841757fb4e",
+    "url": "/InterviewWell/static/js/main.acafd242.chunk.js"
+  },
+  {
+    "revision": "ef29b5a8537ae7ed7e95",
+    "url": "/InterviewWell/static/js/runtime-main.8fcf61d8.js"
+  }
+]);
