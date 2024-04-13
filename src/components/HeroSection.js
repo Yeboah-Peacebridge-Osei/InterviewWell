0891640b57247f1923d82698e4cca03559fe4dfd
Yeboah Peacebridge Osei <73386395+Yeboah-Peacebridge-Osei@users.noreply.github.com>
2024-04-13 16:56:26 -0500

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -8,7 +8,7 @@ function HeroSection() {
     <div className='hero-container'>
       <video src='/videos/video-1.mp4' autoPlay loop muted />
       <h1>YOUR TIME TO INTERVIEW WELL</h1>
-      <p>What are you waiting for?</p>
+      <p>Give us a shot, and see success yourself?</p>
       <div className='hero-btns'>
         <Button
           className='btns'
